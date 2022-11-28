134e6c6bc69fbae8b1896ffd72a45224f06312b7
Edris <e.baz@medrick.info>
2022-11-28 19:45:24 +0330

    init files

---
+++ main.js
@@ -0,0 +1,4 @@
+let _currencies = {"USD" : 35000, "TOMAN":1000}
+let _net_income = 10000
+let _goods = ["coca", "small pizza", "family pizza", "206", "IPhone 13", "galaxy s22 ultra", "condom", "home rent", "hoodie", "wine bottle", "cinema ticket", "laptop 3080", "macbook", "macDonald sandwich", "steam game" ]
+let _times = {"one day" : 1, "one week" : 7, "one month" : 30, "one year" : 365 }
