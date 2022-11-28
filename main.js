bb57bf5ad06a6e66c8b8e9a7f57b8ff6eb3cefe8
Edris <e.baz@medrick.info>
2022-11-28 19:49:15 +0330

    add regions

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-let _currencies = {"USD" : 35000, "TOMAN":1000}
+let _regions = ["Iran", "Netherlands", "Denmark", "Germany"]
+let _currencies = ["USD", "TOMAN"]
+let _currency_values = [35000, 1000]
 let _net_income = 10000
 let _goods = ["coca", "small pizza", "family pizza", "206", "IPhone 13", "galaxy s22 ultra", "condom", "home rent", "hoodie", "wine bottle", "cinema ticket", "laptop 3080", "macbook", "macDonald sandwich", "steam game" ]
 let _times = {"one day" : 1, "one week" : 7, "one month" : 30, "one year" : 365 }
+
+function submit_informations(currency_index, net_income)
+{
+    
+}
